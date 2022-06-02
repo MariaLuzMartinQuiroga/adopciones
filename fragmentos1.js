52f866f1266206abe9f23ff328af59d89b7f58a7
MariaLuzMartinQuiroga <106356069+MariaLuzMartinQuiroga@users.noreply.github.com>
2022-06-01 23:41:44 -0300

    Delete fragmentos1.js

--- fragmentos1.js
+++
@@ -1,1 +0,0 @@
-alert("La presente pagina resulta un ejercicio de practica - La ONG presentada es inexistente, las imagenes fueron tomadas de la web como asi también parte de los textos - SE RESERVAN TODOS LOS DERECHOS");
