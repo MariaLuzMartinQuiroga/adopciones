e534b6264e24e8e6e0c5024ffd1bed4222b8fa3e
MariaLuzMartinQuiroga <106356069+MariaLuzMartinQuiroga@users.noreply.github.com>
2022-06-01 21:14:23 -0300

    Add files via upload

---
+++ fragmentos1.js
@@ -0,0 +1,1 @@
+alert("La presente pagina resulta un ejercicio de practica - La ONG presentada es inexistente, las imagenes fueron tomadas de la web como asi también parte de los textos - SE RESERVAN TODOS LOS DERECHOS");
